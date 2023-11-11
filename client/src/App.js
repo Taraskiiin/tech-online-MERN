bc42b659f4c20f7186d3f7014288b9d9d5a5506f
Taras.Kravets <taraskravets95@gmail.com>
2023-11-11 23:54:29 +0200

    packages: added front-end packages

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,12 @@
+import { ChakraProvider } from "@chakra-ui/react";
+import ProductsScreen from "./screens/ProductsScreen";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <ChakraProvider>
+      <ProductsScreen />
+    </ChakraProvider>
+  );
 }
 
 export default App;
---
+++ client/src/screens/ProductsScreen.jsx
@@ -0,0 +1,8 @@
+import { Box } from "@chakra-ui/react";
+import React from "react";
+
+const ProductsScreen = () => {
+  return <Box>Chackra Box</Box>;
+};
+
+export default ProductsScreen;
