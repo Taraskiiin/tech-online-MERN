c07242bfef189a3c8a9b06b56109eda234c73b94
Taras.Kravets <taraskravets95@gmail.com>
2023-11-20 11:22:53 +0200

    style: added images folder

--- client/src/components/ProductCard.jsx
+++ client/src/components/ProductCard.jsx
@@ -19,7 +19,12 @@ const ProductCard = ({ product, loading }) => {
         overflow="hidden"
         p="4"
         shadow="md">
-        <Image src={product.image[0]} />
+        <Image
+          src={product.images[0]}
+          fallbackSrc="https://via.placeholder.com/150"
+          alt={product.name}
+          height="200px"
+        />
         {product.stock < 5 ? (
           <Badge colorScheme="yellow">only {product.stock} left</Badge>
         ) : product.stock < 1 ? (
